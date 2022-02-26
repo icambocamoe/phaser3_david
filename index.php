6a751274faca5b3a3ba6f822686a6fef1bff6120
icambocamoe <89269526+icambocamoe@users.noreply.github.com>
2022-02-26 14:16:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<html>
-<head>
-    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
-</head>
-<body>
-<?php require 'index.html'; ?>
-</body>
-</html>
+<?php include('index.html'); ?>
